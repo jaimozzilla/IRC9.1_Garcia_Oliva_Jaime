882b16714514ca9ccac15ba9c42a56a4073e5e80
jaimozzilla <olivajaime1710@gmail.com>
2025-02-08 13:03:00 -0600

    Adición de so.py

---
+++ parcial_2/so.py
@@ -0,0 +1,17 @@
+import platform
+import sys
+import subprocess
+
+sistemaop = sys.platform
+sistema = platform.system()
+version = platform.win32_ver()
+
+print("Estamos en {}".format(sistema), " en version: {}".format(version))
+if sistema == 'Windows':
+    local = subprocess.getoutput("""for /f "tokens=2 delims=[]" %a in ('ping -n 1 -4 "%computername%"') do @echo %a""")
+else:
+    local = subprocess.getoutput("ifconfig | grep 'inet' | grep -Fv 127.0.0.1 | awk '{print $2}'")
+print(local)
+print(platform.node())
+print( platform.machine())
+print(platform.processor())
